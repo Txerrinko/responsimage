e74cecbcae6f74f2d45e778485c636c832755dcd
kirkstrobeck <kirk@strobeck.com>
2012-04-18 12:13:04 -0700

    Updates

---
+++ examples/static.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+	<head>
+		
+		<title>responsimage</title>
+
+	</head>
+
+	<body>
+		
+		<img data-responsimage="4884749481_edd0b31c6e_o-1334773151.jpg" class="responsimage" width="<?=$_GET['width']?>" height="<?=$_GET['height']?>" />
+		
+		<script src="http://www.google.com/jsapi" type="text/javascript"></script>
+		<script type="text/javascript">google.load('jquery', '1')</script>
+		<script src="http://raw.github.com/kirkstrobeck/responsimage/master/js/responsimage.min.js"></script>
+
+	</body>
+
+</html>
