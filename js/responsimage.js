ddf235c1814eb8ace003f518b50d43f8a41e8952
kirkstrobeck <kirk@strobeck.com>
2012-04-18 12:09:33 -0700

    Updates

---
+++ js/responsimage.js
@@ -0,0 +1,50 @@
+/*! responsimage v0.1 responsimage.com */
+(
+	function ()
+	{
+		'use strict';
+
+		$(document).ready(
+			function()
+			{
+				function responsimage(init)
+				{
+					$(".responsimage").each(
+						function()
+						{
+							var imgStatic, imgLoading, filename, width, height, imgResponsive;
+
+							if(init)
+							{
+								imgStatic = 'http://f.cl.ly/items/0M3H0q3n1Z1S1y362d09/spacer.gif';
+								imgLoading = 'http://f.cl.ly/items/2w2G3N2p0B400Z380J1u/loading.gif';
+								$(this).attr('src', imgStatic).css('background', '#fff url(' + imgLoading + ') no-repeat center');
+								responsimage();
+							}
+							else
+							{
+								filename = $(this).data('responsimage');
+								width = $(this).width();
+								height = $(this).height();
+
+								if($(this).css('font-family') === 'pixel-ratio-2') { width *= 2; height *= 2; }
+
+								imgResponsive = 'http://imageizer.com/image/2/' + width + '/' + height + '/5/0/uploads/' + filename;
+								$(this).attr('src', imgResponsive);
+							}
+						}
+					);
+				}
+
+				responsimage(1);
+
+				$(window).resize(
+					function()
+					{
+						responsimage();
+					}
+				);
+			}
+		);
+	}()
+);
