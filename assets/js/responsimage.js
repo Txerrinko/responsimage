808f875c669038256d18d65e5418081d4d7d2ffb
kirkstrobeck <kirk@strobeck.com>
2012-04-24 22:42:46 -0700

    Added version line, web reference, converted spaces to tabs

--- assets/js/responsimage.js
+++ assets/js/responsimage.js
@@ -1,54 +1,55 @@
+/* responsimage.com v0.2 */
 (function ($, window, Date) {
 
-    'use strict';
+	'use strict';
 
-    $(function() {
+	$(function() {
 
-        var rPrefs = $('meta[name=\'responsimage\']'),
-            rServer = rPrefs.data('server'),
-            rStatic = rPrefs.data('static') || 'http://f.cl.ly/items/0M3H0q3n1Z1S1y362d09/spacer.gif',
-            rLoading = rPrefs.data('loading') || 'http://f.cl.ly/items/2w2G3N2p0B400Z380J1u/loading.gif',
-            rLimit = rPrefs.data('limit') || 100,
-            rTimestamp = Date.now(),
-            rTags = $('.responsimage');
+		var rPrefs = $('meta[name=\'responsimage\']'),
+			rServer = rPrefs.data('server'),
+			rStatic = rPrefs.data('static') || 'http://f.cl.ly/items/0M3H0q3n1Z1S1y362d09/spacer.gif',
+			rLoading = rPrefs.data('loading') || 'http://f.cl.ly/items/2w2G3N2p0B400Z380J1u/loading.gif',
+			rLimit = rPrefs.data('limit') || 100,
+			rTimestamp = Date.now(),
+			rTags = $('.responsimage');
 
-        function responsimage(rInit) {
-            rTags.each(function() {
-                var rThis = $(this),
-                    filename = rThis.data('responsimage'),
-                    rWidth = rThis.width(),
-                    rHeight = rThis.height(),
-                    rImage;
+		function responsimage(rInit) {
+			rTags.each(function() {
+				var rThis = $(this),
+					filename = rThis.data('responsimage'),
+					rWidth = rThis.width(),
+					rHeight = rThis.height(),
+					rImage;
 
-                if(rInit) {
-                    rThis.attr('src', rStatic).css('background', '#fff url(' + rLoading + ') no-repeat center');
-                }
+				if(rInit) {
+					rThis.attr('src', rStatic).css('background', '#fff url(' + rLoading + ') no-repeat center');
+				}
 
-                if(rThis.css('font-family') === 'pixel-ratio-2') {
-                    rWidth *= 2;
-                    rHeight *= 2;
-                }
+				if(rThis.css('font-family') === 'pixel-ratio-2') {
+					rWidth *= 2;
+					rHeight *= 2;
+				}
 
-                rImage = rServer.replace('width', rWidth).replace('height', rHeight).replace('filename', filename);
+				rImage = rServer.replace('width', rWidth).replace('height', rHeight).replace('filename', filename);
 
-                if(filename !== 'disabled') {
-                    rThis.attr('src', rImage);
-                }
-            });
-        }
+				if(filename !== 'disabled') {
+					rThis.attr('src', rImage);
+				}
+			});
+		}
 
-        responsimage(1);
+		responsimage(1);
 
-        $(window).resize(function () {
-            var rNow = Date.now();
+		$(window).resize(function () {
+			var rNow = Date.now();
 
-            if (rNow - rTimestamp >= rLimit) {
-                responsimage(false);
-            }
-        });
+			if (rNow - rTimestamp >= rLimit) {
+				responsimage(false);
+			}
+		});
 
-        window.onorientationchange = function() {      
-            setTimeout(responsimage, 0); 
-        };
-    });
+		window.onorientationchange = function() {      
+			setTimeout(responsimage, 0); 
+		};
+	});
 }(jQuery, window, Date));
