54f860158eaed283a6bea0152ba3461ef102a5c4
kirkstrobeck <kirk@strobeck.com>
2012-04-25 02:37:37 -0700

    update misc

--- assets/js/responsimage.js
+++ assets/js/responsimage.js
@@ -7,9 +7,9 @@
 
 		var rPrefs = $('meta[name="responsimage"]'),
 			rServer = rPrefs.data('server'),
-			rStatic = 'http://f.cl.ly/items/0M3H0q3n1Z1S1y362d09/spacer.gif',
-			rLoading = 'http://f.cl.ly/items/2w2G3N2p0B400Z380J1u/loading.gif',
-			rLimit = 100,
+			rStatic = rPrefs.data('static') || 'http://f.cl.ly/items/0M3H0q3n1Z1S1y362d09/spacer.gif',
+			rLoading = rPrefs.data('loading') || 'http://f.cl.ly/items/2w2G3N2p0B400Z380J1u/loading.gif',
+			rLimit = rPrefs.data('limit') || 100,
 			rTimestamp = Date.now(),
 			rTags = $('.responsimage');
 
